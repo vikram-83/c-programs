b83d2fabd5a86ac1cf6b7d63af50325020128441
vikram-83 <vikramchaudhari834@gmail.com>
2024-07-31 21:32:40 +0530

    click here to download
    program to find factorial of a given number.

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+int main()    
+
+
+int i,fact=1,enter;
+printf("enter a number :") ;
+scanf("%d",&number);
+for(i=1;i<=number;i++)
+fact=fact*i;
+printf ("factorial of %d is :%d",number, fact);
+}
+
