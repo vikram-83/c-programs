29d1f86379d6f357043968b96a9c58e2b14ecf09
vikram-83 <vikramchaudhari834@gmail.com>
2024-08-05 21:06:04 +0530

    click here to see code

---
+++ first-progrsm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main (){
+        int a[5];
+        printf("enter the ALIMENT ");
+        scanf("%d",)
+    
+        for(int i=0;i<=5;i++){
+
+        scanf("%d",&a[i]);
+    }
+    
+
+
+
+    
+    }
+ 
